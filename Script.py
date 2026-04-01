abfd6056b51d99ff3553b554f4703caac60f6677
Stephinhh <wonderland2000azz@gmail.com>
2026-04-01 19:47:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -222,7 +222,7 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
-</b><a href={poster_url}>📥</a><a href={imdb_url}>ɴᴇᴡ {tag}    ʜᴅ ᴜᴘʟᴏᴀᴅᴇᴅ✅</a></b>
+</b><a href={poster_url}>📥</a><a href={imdb_url}>ɴᴇᴡ {tag}   ʜᴅ ᴜᴘʟᴏᴀᴅᴇᴅ✅</a></b>
 
 <blockquote>🎬 ᴛɪᴛʟᴇ : <code>{filename}</code>
 
