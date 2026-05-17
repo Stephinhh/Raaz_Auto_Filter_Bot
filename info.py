9674f741a9c1fb4de01d5b27752dd7e0970746d1
Stephinhh <wonderland2000azz@gmail.com>
2026-05-17 17:42:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003943817506')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/lolansupport')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003723172874")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1003952359146")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1002290170333")# Channels for force sub (make sure bot is admin)
 
 # ============================
